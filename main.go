cdc7e824a2e7027c1e1d0c5b87e6a494c4309e88
Liam Zdenek <liamzdenek@gmail.com>
2013-04-12 00:53:43 -0500

    Adding basic response functionality

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import "fmt";
-import "reflect";
 
 func main() {
     conn := *NewM2Connection("82209006-86FF-4982-B5EA-D1E29E55D481", "tcp://127.0.0.1:9997", "tcp://127.0.0.1:9996");
-    
-    fmt.Printf("%s\n", reflect.TypeOf(conn));
+
+    fmt.Printf("Starting\n");
     for {
-        ret := conn.poll();
-        if ret != nil {
-            fmt.Printf("%v\n", ret.headers);
+        req := conn.poll();
+        if req != nil {
+            //fmt.Printf("%v\n", req.headers[len(req.headers)-1].value);
+            req.reply("HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nlol");
         }
     }
 }
